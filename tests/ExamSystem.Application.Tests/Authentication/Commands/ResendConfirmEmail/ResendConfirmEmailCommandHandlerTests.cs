d4be4c17ca64406f057a2302c3447c4bf49b9239
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2026-01-14 14:23:22 +0200

    test(auth): add unit tests for resend confirm email feature (Handler, Validator)

---
+++ tests/ExamSystem.Application.Tests/Authentication/Commands/ResendConfirmEmail/ResendConfirmEmailCommandHandlerTests.cs
@@ -0,0 +1,90 @@
+﻿using ExamSystem.Application.Contracts.ExternalServices;
+using ExamSystem.Application.Contracts.Services;
+using ExamSystem.Application.Features.Authentication.Commands.ResendConfirmEmail;
+using ExamSystem.Application.Tests.Helpers;
+using ExamSystem.Domain.Entities;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using System.Linq.Expressions;
+
+namespace ExamSystem.Application.Tests.Authentication.Commands.ResendConfirmEmail
+{
+
+
+    public class ResendConfirmEmailCommandHandlerTests
+    {
+        private readonly Mock<IAppEmailService> _emailServiceMock;
+        private readonly Mock<IBackgroundJobService> _backgroundJobMock;
+        private readonly Mock<UserManager<ApplicationUser>> _userManagerMock;
+        private readonly ResendConfirmEmailCommandHandler _handler;
+
+        public ResendConfirmEmailCommandHandlerTests()
+        {
+            _emailServiceMock = new Mock<IAppEmailService>();
+            _backgroundJobMock = new Mock<IBackgroundJobService>();
+            _userManagerMock = MockHelper.CreateUserManagerMock<ApplicationUser>();
+
+            _handler = new ResendConfirmEmailCommandHandler(_emailServiceMock.Object, _backgroundJobMock.Object, _userManagerMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_UserNotFound_ReturnsOkWithoutSendAnyThing()
+        {
+            // Arrange
+            var command = new ResendConfirmEmailCommand("test@test.com");
+            _userManagerMock.Setup(x => x.FindByEmailAsync(command.Email)).ReturnsAsync((ApplicationUser)null!);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Handle_EmailAlreadyConfirmed_ReturnsValidationError()
+        {
+            // Arrange
+            var user = new ApplicationUser
+            {
+                Email = "confirmed@test.com",
+                EmailConfirmed = true
+            };
+
+            var command = new ResendConfirmEmailCommand(user.Email);
+            _userManagerMock.Setup(x => x.FindByEmailAsync(command.Email)).ReturnsAsync(user);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Handle_UserNotConfirmed_EnqueuesEmailAndReturnsOk()
+        {
+            // Arrange
+            var user = new ApplicationUser
+            {
+                Email = "ali@test.com",
+                EmailConfirmed = false
+            };
+
+            var command = new ResendConfirmEmailCommand(user.Email);
+            _userManagerMock.Setup(x => x.FindByEmailAsync(command.Email)).ReturnsAsync(user);
+
+            _userManagerMock.Setup(x => x.GenerateEmailConfirmationTokenAsync(user)).ReturnsAsync("fake-token");
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+
+            _backgroundJobMock.Verify(x => x.Enqueue(It.IsAny<Expression<Action>>()), Times.Once);
+        }
+    }
+}
+
---
+++ tests/ExamSystem.Application.Tests/Authentication/Commands/ResendConfirmEmail/ResendConfirmEmailCommandValidatorTests.cs
@@ -0,0 +1,39 @@
+﻿using ExamSystem.Application.Features.Authentication.Commands.ResendConfirmEmail;
+using FluentAssertions;
+
+namespace ExamSystem.Application.Tests.Authentication.Commands.ResendConfirmEmail
+{
+    public class ResendConfirmEmailCommandValidatorTests
+    {
+        private readonly ResendConfirmEmailCommandValidator _validator = new ResendConfirmEmailCommandValidator();
+
+        [Theory]
+        [InlineData(null)]                                              // email is null
+        [InlineData("")]                                                // email is empty
+        [InlineData("invalid-email-format")]                            // invalid email format 
+        public void Validate_ShouldHaveError_WhenInvalidInputs(string email)
+        {
+            // Arrange
+            var command = new ResendConfirmEmailCommand(email);
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Validate_ShouldPass_WhenInputIsValid()
+        {
+            // Arrange
+            var command = new ResendConfirmEmailCommand("test@gmail.com");
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
